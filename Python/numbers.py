074ffc93222f8111766c0ed5de6f19ec2a3c6f06
nbobrov8 <bobrovn8@yandex.ru>
2021-03-23 19:49:11 +0300

    Python files

---
+++ Python/numbers.py
@@ -0,0 +1,11 @@
+print('Enter 4 numbers: ')
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+num_3 = float(input("Enter third number: "))
+num_4 = float(input("Enter fourth number: "))
+
+s1 = num_1 + num_2
+s2 = num_3 + num_4
+result = s1 / s2
+
+print("%.2f" % result)
